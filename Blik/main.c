52fb433b3655a7ed308d338d5897f3b8dde63d40
Vojtech Navratil <navryvoj@gmail.com>
2024-01-09 12:59:41 +0100

    initial commit

---
+++ Blik/main.c
@@ -0,0 +1,34 @@
+/*
+ * main.c
+ *
+ * Created: 1/9/2024 12:43:45 PM
+ *  Author: vn
+ */ 
+
+#include <xc.h>
+
+#include <avr/interrupt.h>
+
+#define LED PB0
+
+ISR(TIM0_OVF_vect) {
+	// Toggle port B pin 4 output state
+	PORTB ^= _BV(LED);
+}
+
+int main(void) {
+	// Set port B output 0 as output
+	DDRB = _BV(LED);
+
+	// Prescale timer to 1/1024th the clock rate
+	TCCR0B |= _BV(CS02) | _BV(CS00);
+
+	// Enable timer overflow interrupt
+	TIMSK0 |=_BV(TOIE0);
+
+	// Enable interrupts
+	sei();
+
+	// Loop forever
+	while(1) {}
+}
