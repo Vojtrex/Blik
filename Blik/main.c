2370675be82c1e5cb08220d88adf9ba96b41ae89
Vojtech Navratil <navryvoj@gmail.com>
2024-01-09 13:08:59 +0100

    merging from local repo

--- Blik/main.c
+++ Blik/main.c
@@ -9,16 +9,16 @@
 
 #include <avr/interrupt.h>
 
-#define LED PB0
+#define LEDPIN1 PB0
 
 ISR(TIM0_OVF_vect) {
 	// Toggle port B pin 4 output state
-	PORTB ^= _BV(LED);
+	PORTB ^= _BV(LEDPIN1);
 }
 
 int main(void) {
 	// Set port B output 0 as output
-	DDRB = _BV(LED);
+	DDRB = _BV(LEDPIN1);
 
 	// Prescale timer to 1/1024th the clock rate
 	TCCR0B |= _BV(CS02) | _BV(CS00);
