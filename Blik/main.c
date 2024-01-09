6813454668f04a130e65d0d69264ff1b8ecd9879
Vojtech Navratil <navryvoj@gmail.com>
2024-01-09 15:22:54 +0100

    device changed to Attiny85

--- Blik/main.c
+++ Blik/main.c
@@ -1,43 +1,79 @@
 /*
- * main.c
- *
- * Created: 1/9/2024 12:43:45 PM
- *  Author: vn
- */ 
+* main.c
+*
+* Created: 1/9/2024 12:43:45 PM
+*  Author: vn
+*/
+
+#define F_CPU 1000000UL
 
 #include <xc.h>
 #include <avr/interrupt.h>
+#include <avr/sleep.h>
+#include <stdbool.h>
+#include <util/delay.h>
 
-#define LEDPIN1 PB0
+#define LEDPIN1 PINB3
+#define LEDPIN2 PINB4
+#define BUTTONPIN PINB5
+
+bool button_pressed = false;
 
 ISR(TIM0_OVF_vect) {
-	// Toggle port B pin 4 output state
-	PORTB ^= _BV(LEDPIN1);
+	// Toggle buton pressed boolean
+	button_pressed = true;
 }
 
-ISR(PCINT0_vect){
-	
+ISR(PCINT0_vect) {
+	if (PINB & _BV(BUTTONPIN))
+	// Toggle buton pressed boolean
+	button_pressed = true;
 }
 
 void init (){
-		// Set port B output 0 as output
-		DDRB = _BV(LEDPIN1);
+	// Set B ports as outputs
+	DDRB = _BV(LEDPIN1);
+	DDRB = _BV(LEDPIN2);
+	
+	
+	CKSEL =
+	
+	// Prescale timer to 1/1024th the clock rate
+	TCCR0B |= _BV(CS02) | _BV(CS00);
 
-		// Prescale timer to 1/1024th the clock rate
-		TCCR0B |= _BV(CS02) | _BV(CS00);
-
-		// Enable timer overflow interrupt
-		TIMSK0 |=_BV(TOIE0);
+	// Enable timer overflow interrupt
+	//TIMSK |=_BV(TOIE0);
+	
+	PCMSK |= _BV(BUTTONPIN);   // Set pin change interrupt mask to listen to port b3
+	
+	MCUCR = _BV(ISC01) | _BV(ISC00);    // Set interrupt on INT0 pin falling edge (high pulled to low)
+	
+	//Set sleep mode to power down
+	set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+	
+	// Enable interrupts
+	GIMSK |= _BV(PCIE);     // Enable PCINT interrupt
+	sei();
 	
 }
 
 int main(void) {
 	//Initialize
 	init();
-	
-	// Enable interrupts
-	sei();
 
-	// Loop forever
-	while(1) {}
+	for (;;) {
+		
+		if (button_pressed)
+		{
+			for (uint8_t i = 0; i < 3; i++)
+			{
+				PORTB ^= _BV(LEDPIN1);
+				_delay_ms(1000UL);
+				PORTB ^= _BV(LEDPIN1);
+				_delay_ms(2000UL);
+			}
+			button_pressed = false;
+		}
+		sleep_mode();
+	}
 }
